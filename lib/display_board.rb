ce3f3a533ea90b7d7aa6652d41db649df20154b1
Cory Pavitt <corypavitt@hotmail.com>
2018-06-20 16:20:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "Welcome to Tic Tac Toe"
+  display_board
 end
- row1 = []
- row1 = [' ',' ', ' ']
- puts row1
+
